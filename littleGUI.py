b68590bded888387de3970ee11101f685ba28ab3
rorjackson <46418690+rorjackson@users.noreply.github.com>
2019-01-22 21:56:46 +0500

    Add files via upload

---
+++ littleGUI.py
@@ -0,0 +1,12 @@
+# from tkinter import *
+# root=Tk()
+# theLabel = Label(root, text="This is too easy")
+# theLabel.pack()
+# root.mainloop()
+
+
+from tkinter import *
+root = Tk()
+thelable= Label(root, text = "This is not very easy hehehe")
+thelable.pack()
+root.mainloop()
